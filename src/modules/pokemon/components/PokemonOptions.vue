f0ddca3af0ad54676e2f9e35cfebce7f56f3c905
Juan Florez <juanks339@gmail.com>
2024-10-02 16:09:10 -0500

    finish game

--- src/modules/pokemon/components/PokemonOptions.vue
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="mt-5">
-    <ul>
-      <li>Pokemon 1</li>
-      <li>Pokemon 2</li>
-      <li>Pokemon 3</li>
-      <li>Pokemon 4</li>
-    </ul>
+  <div class="mt-5 flex flex-col">
+    <button
+      v-for="{ name, id } in options"
+      :key="id"
+      @click="$emit('selectedOption', id)"
+      :class="[
+        'capitalize disabled:bg-gray-100',
+        {
+          correct: id === correctAnswer && blockSelection,
+          incorrect: id !== correctAnswer && blockSelection,
+        },
+      ]"
+      :disabled="blockSelection"
+    >
+      {{ name }}
+    </button>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import type { Pokemon } from '../interfaces';
+
+interface Props {
+  options: Pokemon[];
+  correctAnswer: number;
+  blockSelection: boolean;
+}
+
+defineProps<Props>();
+
+defineEmits<{
+  selectedOption: [id: number];
+}>();
+</script>
 
 <style scoped>
-li {
+button {
   @apply bg-white shadow-md rounded-lg p-3 m-2 cursor-pointer w-40 text-center transition-all hover:bg-gray-100;
+}
+
+.correct {
+  @apply bg-blue-500 text-white;
+}
+
+.incorrect {
+  @apply bg-red-500 opacity-70;
 }
 </style>
--- src/modules/pokemon/components/PokemonPicture.vue
+++ src/modules/pokemon/components/PokemonPicture.vue
@@ -1,14 +1,28 @@
+import { computed } from 'vue';
 <template>
   <div>
-    <img
-      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg"
-      alt="dream-world"
-      class="brightness-0 h-[200px]"
-    />
+    <img v-if="!showPokemon" :src="pokemonImg" class="brightness-0 h-[200px]" />
+    <img v-else :src="pokemonImg" class="fade-in h-[200px]" alt="pokemon image" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  pokemonId: number;
+  showPokemon?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  showPokemon: false,
+});
+
+const pokemonImg = computed(() => {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.pokemonId}.svg`;
+});
+</script>
+
 <style scoped>
 img {
   user-select: none;
